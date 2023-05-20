20790f3f4c5620a425f9438c7e28f57206914287
glenrioariesto <glenrioariesto@gmail.com>
2023-05-20 20:42:35 +0700

    test sonalcloud

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -5,14 +5,14 @@ public class Driver {
 	public static void main(String[] args) {
 		
 		Counter counter = new Counter();
+		counter.increment(5);
 		
 		System.out.println("Hasil:" + counter.getCount());
 		
-		counter.increment();
-		
+		counter.decrement(2);
 		System.out.println("Hasil:" + counter.getCount());
 		
-		counter.decrement();
+		
 		
 		System.out.println("Hasil:" + counter.getCount());
 		
