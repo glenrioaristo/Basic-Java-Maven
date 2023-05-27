e969384f6b07727dbc108ac707d7b34b06d64aca
glenrioariesto <glenrioariesto@gmail.com>
2023-05-27 22:50:00 +0700

    perbaiki code

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -5,12 +5,11 @@ public class Driver {
 	public static void main(String[] args) {
 		
 		Counter counter = new Counter();
-		counter.increment(5);
-		
+
 		System.out.println("Hasil:" + counter.getCount());
 		
 		System.out.println("Hasil:" + counter.getCount());
-		counter.decrement(2);
+		
 		System.out.println("Hasil:" + counter.getCount());
 		
 		
